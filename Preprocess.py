b5a84d28ddff7f8dcb7255d6090c8113296f2111
omer6nahum <omer6nahum@gmail.com>
2021-08-07 19:42:45 +0300

    Changed functions in Preprocess to work with less players in midfield

--- Preprocess.py
+++ Preprocess.py
@@ -4,7 +4,7 @@ import pickle
 from tqdm import tqdm
 
 
-def create_squad(team_name, year, k=None):
+def create_squad(team_in_season_df, k=None):
     """
     :param team_name:
     :param year: season year (2 figures, e.g: 15)
@@ -13,39 +13,36 @@ def create_squad(team_name, year, k=None):
     """
 
     if k is None:
-        # k = {'gk': 2,
-        #      'def': 6,
-        #      'mid': 4,
-        #      'att': 6}
-        k = {'gk': 1,
-             'def': 2,
-             'mid': 2,
-             'att': 2}
+        k = {'gk': 2,
+             'def': 6,
+             'mid': 5,
+             'att': 5}
 
     features_pickle_path = 'data/pickles/features_dict.pkl'
     with open(features_pickle_path, 'rb') as f:
         features_dict = pickle.load(f)
 
-    path = f'data/players/players_{year}.csv'
-    data = pd.read_csv(path)
+    data = team_in_season_df.drop(['club_name', 'sofifa_id', 'best_position', 'short_name'], axis=1)
 
-    data = data[data['club_name'] == team_name].drop(['club_name', 'sofifa_id', 'best_position', 'short_name'], axis=1)
+    gk_data, _, __ = create_position_category_data(data, 'gk', k['gk'], features_dict['field_skills'])
+    def_data, def_rest, __ = create_position_category_data(data, 'def', k['def'], features_dict['gk_skills'])
+    mid_data, _, num_returned = create_position_category_data(data, 'mid', k['mid'], features_dict['gk_skills'])
+    att_data, att_rest, __ = create_position_category_data(data, 'att', k['att'], features_dict['gk_skills'])
 
-    gk_data = create_position_category_data(data, 'gk', k['gk'], features_dict['field_skills'])
-    def_data = create_position_category_data(data, 'def', k['def'], features_dict['gk_skills'])
-    mid_data = create_position_category_data(data, 'mid', k['mid'], features_dict['gk_skills'])
-    att_data = create_position_category_data(data, 'att', k['att'], features_dict['gk_skills'])
-
-    # todo: complete from another category if head is not enough players
+    if num_returned < k['mid']:
+        rest_data = pd.concat((def_rest, att_rest)).sort_values(by='overall', ascending=False)
+        rest_data = np.array(rest_data.head(k['mid'] - num_returned).values.flatten())
+        mid_data = np.hstack((mid_data, rest_data))
 
     return np.concatenate((gk_data, def_data, mid_data, att_data))
 
 
 def create_position_category_data(data, position_category, k, features_to_drop):
-    return np.array(data[data['position_category'] == position_category] \
-        .drop(['position_category'] + features_to_drop, axis=1) \
-        .sort_values(by='overall', ascending=False) \
-        .head(k).values.flatten())
+    res = data[data['position_category'] == position_category] \
+         .drop(['position_category'] + features_to_drop, axis=1) \
+         .sort_values(by='overall', ascending=False)
+
+    return np.array(res.head(k).values.flatten()), res.tail(res.shape[0] - k), min(res.shape[0], k)
 
 
 def create_x_y_z_approach1(year):
@@ -56,11 +53,11 @@ def create_x_y_z_approach1(year):
     y = []  # y_i is number of points
     z = []  # z_i is team name
 
+    path = f'data/players/players_{year}.csv'
+    data = pd.read_csv(path)
+
     for i, row in table.iterrows():
-        x_i = create_squad(row['team_name'], year)  # x_i is a team's squad
-        if len(x_i) == 0:
-            print(row['team_name'])
-        x.append(x_i)
+        x.append(create_squad(data[data['club_name'] == row['team_name']]))  # x_i is a team's squad
         y.append(float(row['PTS']))  # y_i is number of points at the end of the season
         z.append(row['team_name'])
     return np.vstack(x), np.vstack(y), np.vstack(z)
@@ -74,9 +71,12 @@ def create_x_y_z_approach2(year):
     y = []  # y_i is label from {'H', 'D', 'A'}
     z = []  # z_i is 2d vector: [home_team_name, away_team_name]
 
+    path = f'data/players/players_{year}.csv'
+    data = pd.read_csv(path)
+
     for i, row in all_matches.iterrows():
-        home_squad = create_squad(row['HomeTeam'], year)
-        away_squad = create_squad(row['AwayTeam'], year)
+        home_squad = create_squad(data[data['club_name'] == row['HomeTeam']])
+        away_squad = create_squad(data[data['club_name'] == row['AwayTeam']])
         squads = np.concatenate((home_squad, away_squad))
         x.append(squads)
         y.append(row['FTR'])
@@ -133,5 +133,6 @@ if __name__ == '__main__':
     print(f'x={x_train.shape}, y={y_train.shape}, z={z_train.shape}')
     x_train, x_test, y_train, y_test, z_train, z_test = create_train_test(test_year=21, approach=2)
     print(f'x={x_train.shape}, y={y_train.shape}, z={z_train.shape}')
+
 
 
