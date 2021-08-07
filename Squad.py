25cce96a82c5600167a6f0849108547e9a67a849
omer6nahum <omer6nahum@gmail.com>
2021-08-07 18:18:58 +0300

    Added function for first approach x,y,z creation

--- Squad.py
+++ Squad.py
@@ -12,10 +12,14 @@ def create_squad(team_name, year, k=None):
     """
 
     if k is None:
-        k = {'gk': 2,
-             'def': 6,
-             'mid': 4,
-             'att': 6}
+        # k = {'gk': 2,
+        #      'def': 6,
+        #      'mid': 4,
+        #      'att': 6}
+        k = {'gk': 1,
+             'def': 2,
+             'mid': 2,
+             'att': 2}
 
     features_pickle_path = 'data/pickles/features_dict.pkl'
     with open(features_pickle_path, 'rb') as f:
@@ -31,15 +35,76 @@ def create_squad(team_name, year, k=None):
     mid_data = create_position_category_data(data, 'mid', k['mid'], features_dict['gk_skills'])
     att_data = create_position_category_data(data, 'att', k['att'], features_dict['gk_skills'])
 
+    # todo: complete from another category if head is not enough players
+
     return np.concatenate((gk_data, def_data, mid_data, att_data))
 
 
 def create_position_category_data(data, position_category, k, features_to_drop):
-    return data[data['position_category'] == position_category] \
+    return np.array(data[data['position_category'] == position_category] \
         .drop(['position_category'] + features_to_drop, axis=1) \
         .sort_values(by='overall', ascending=False) \
-        .head(k).values.flatten()
+        .head(k).values.flatten())
+
+
+def create_x_y_z_approach1(year):
+    table_path = f'data/tables/table_{year}.csv'
+    table = pd.read_csv(table_path)
+    # todo: document x,y,z
+    x = []
+    y = []
+    z = []
+    for i, row in table.iterrows():
+        x_i = create_squad(row['team_name'], year)  # x_i is a team's squad
+        if len(x_i) == 0:
+            print(row['team_name'])
+        x.append(x_i)
+        y.append(float(row['PTS']))  # y_i is number of points at the end of the season
+        z.append(row['team_name'])
+    return np.vstack(x), np.vstack(y), np.vstack(z)
+
+
+def create_train_test_approach1(test_year):
+    years = range(15, 22)
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+    z_train = []
+    z_test = []
+    for year in years:
+        x, y, z = create_x_y_z_approach1(year)
+        if year == test_year:
+            x_test.append(x)
+            y_test.append(y)
+            z_test.append(z)
+        else:
+            x_train.append(x)
+            y_train.append(y)
+            z_train.append(z)
+    x_train = np.vstack(x_train)
+    y_train = np.vstack(y_train)
+    z_train = np.vstack(z_train)
+    x_test = np.vstack(x_test)
+    y_test = np.vstack(y_test)
+    z_test = np.vstack(z_test)
+
+    return x_train, x_test, y_train, y_test, z_train, z_test
+
+
+def create_x_y_z_approach2(year):
+    # todo: finish
+    all_matches_path = f'data/matches/matches_{year}.csv'
+    all_matches = pd.read_csv(all_matches_path)
+    teams = list(set(all_matches['HomeTeam']))
+
+
+def create_train_test_approach2(test_years):
+    # todo: finish
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == '__main__':
-    print(create_squad('Liverpool', 21).shape)
+    x_train, x_test, y_train, y_test, z_train, z_test = create_train_test_approach1(21)
+
+
