0073c1f1fba9f52f8e5e9ae41c607094beafbb7f
omer6nahum <omer6nahum@gmail.com>
2021-08-07 18:57:41 +0300

    Changed data/matches directory Added function for second approach x,y,z creation

--- Preprocess.py
+++ Preprocess.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle
+from tqdm import tqdm
 
 
 def create_squad(team_name, year, k=None):
@@ -50,10 +51,11 @@ def create_position_category_data(data, position_category, k, features_to_drop):
 def create_x_y_z_approach1(year):
     table_path = f'data/tables/table_{year}.csv'
     table = pd.read_csv(table_path)
-    # todo: document x,y,z
-    x = []
-    y = []
-    z = []
+
+    x = []  # x_i is a squad representation
+    y = []  # y_i is number of points
+    z = []  # z_i is team name
+
     for i, row in table.iterrows():
         x_i = create_squad(row['team_name'], year)  # x_i is a team's squad
         if len(x_i) == 0:
@@ -64,7 +66,41 @@ def create_x_y_z_approach1(year):
     return np.vstack(x), np.vstack(y), np.vstack(z)
 
 
-def create_train_test_approach1(test_year):
+def create_x_y_z_approach2(year):
+    all_matches_path = f'data/matches/matches_{year}.csv'
+    all_matches = pd.read_csv(all_matches_path)
+
+    x = []  # x_i is 2-squads representation
+    y = []  # y_i is label from {'H', 'D', 'A'}
+    z = []  # z_i is 2d vector: [home_team_name, away_team_name]
+
+    for i, row in all_matches.iterrows():
+        home_squad = create_squad(row['HomeTeam'], year)
+        away_squad = create_squad(row['AwayTeam'], year)
+        squads = np.concatenate((home_squad, away_squad))
+        x.append(squads)
+        y.append(row['FTR'])
+        z.append(np.array([row['HomeTeam'], row['AwayTeam']]))
+
+    return np.vstack(x), np.vstack(y), np.vstack(z)
+
+
+def create_x_y_z(year, approach):
+    """
+    :param year: 2 digits
+    :param approach: can be one of {1, 2}
+    :return:
+    """
+
+    if approach == 1:
+        return create_x_y_z_approach1(year)
+    elif approach == 2:
+        return create_x_y_z_approach2(year)
+    else:
+        raise ValueError('Not an approach')
+
+
+def create_train_test(test_year, approach):
     years = range(15, 22)
     x_train = []
     y_train = []
@@ -72,8 +108,8 @@ def create_train_test_approach1(test_year):
     y_test = []
     z_train = []
     z_test = []
-    for year in years:
-        x, y, z = create_x_y_z_approach1(year)
+    for year in tqdm(years):
+        x, y, z = create_x_y_z(year, approach)
         if year == test_year:
             x_test.append(x)
             y_test.append(y)
@@ -92,19 +128,10 @@ def create_train_test_approach1(test_year):
     return x_train, x_test, y_train, y_test, z_train, z_test
 
 
-def create_x_y_z_approach2(year):
-    # todo: finish
-    all_matches_path = f'data/matches/matches_{year}.csv'
-    all_matches = pd.read_csv(all_matches_path)
-    teams = list(set(all_matches['HomeTeam']))
-
-
-def create_train_test_approach2(test_years):
-    # todo: finish
-    return X_train, X_test, y_train, y_test
-
-
 if __name__ == '__main__':
-    x_train, x_test, y_train, y_test, z_train, z_test = create_train_test_approach1(21)
+    x_train, x_test, y_train, y_test, z_train, z_test = create_train_test(test_year=21, approach=1)
+    print(f'x={x_train.shape}, y={y_train.shape}, z={z_train.shape}')
+    x_train, x_test, y_train, y_test, z_train, z_test = create_train_test(test_year=21, approach=2)
+    print(f'x={x_train.shape}, y={y_train.shape}, z={z_train.shape}')
 
 
