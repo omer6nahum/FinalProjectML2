9c5e152d4b8250f771ed452ec7ab08923223d1f9
omer6nahum <omer6nahum@gmail.com>
2021-08-18 06:39:47 +0300

    Optimized the BasicNN model via grid search. Results are in grid_search_results.txt

--- OptimizeBasicNN.py
+++ OptimizeBasicNN.py
@@ -7,6 +7,7 @@ import numpy as np
 from tqdm import tqdm
 from main import second_approach_cv
 from itertools import product
+import pickle
 
 if __name__ == '__main__':
     test_years = range(15, 22)
@@ -15,9 +16,9 @@ if __name__ == '__main__':
     input_shape = load_x_y_z_pickle(15, 2)[0].shape[1]
     metrics = []
 
-    lr_list = [1e-2, 1e-3, 1e-5]
+    lr_list = [1e-3, 1e-4, 1e-5]
     num_epochs_list = [5, 10, 20, 50]
-    batch_size_list = [4, 32, 128]
+    batch_size_list = [32, 64, 128]
     num_units_list = [input_shape//2, input_shape//8, input_shape//64]
     adj_acc_dict = dict()
     for (lr, num_epochs, batch_size, num_units) in tqdm(product(lr_list, num_epochs_list, batch_size_list, num_units_list)):
@@ -27,6 +28,11 @@ if __name__ == '__main__':
         adj_acc = second_app_model_result['expectation']['adj_accuracy']
         adj_acc_dict[(lr, num_epochs, batch_size, num_units)] = adj_acc
 
+    print(adj_acc_dict)
+    with open('optimized_basic_nn.pkl', 'wb') as f:
+        pickle.dump(adj_acc_dict, f)
+
+    print('\n\n\n-------------------------------------')
     for k, v in sorted(adj_acc_dict.items(), key=lambda x: x[1], reverse=True):
         print(f'{k} ::: {v:4f}')
 
